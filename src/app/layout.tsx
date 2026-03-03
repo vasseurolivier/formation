bc7009a1d0e87b5f66b9ebe8629ecb2e7b368e43
Olivier Vasseur <oliviervasseur0707@gmail.com>
2026-03-03 09:33:58 +0000

    le nom du site est : PERPECTIVE PRO FORMATION corrige partout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import Header from '@/components/header';
 import Footer from '@/components/footer';
 
 export const metadata: Metadata = {
-  title: 'Global Connect Academy',
+  title: 'PERPECTIVE PRO FORMATION',
   description: 'Your Gateway to an International Career',
 };
 
