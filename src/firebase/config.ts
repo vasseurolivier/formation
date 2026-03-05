21a58bd43cafe3950d2f37576e97b8218751840b
Olivier Vasseur <oliviervasseur0707@gmail.com>
2026-03-05 09:10:51 +0000

    le televersement d une image ne fonctionne pas, probleme de connexion ?

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:713914158883:web:09b0820df094b14c494696",
   "apiKey": "AIzaSyBPGIhdR8EQqNLdICmw5Uqlv1lFb-pkj6s",
   "authDomain": "studio-3353991594-a91d2.firebaseapp.com",
+  "storageBucket": "studio-3353991594-a91d2.appspot.com",
   "measurementId": "",
   "messagingSenderId": "713914158883"
 };
