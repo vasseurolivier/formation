b48b00bf06c9daaa4ed6476250f4325780c0b77d
Olivier Vasseur <oliviervasseur0707@gmail.com>
2026-03-06 03:06:48 +0000

    l'espace prevu pour le logo est trop large, mon logo ne prends que la pa

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -67,7 +67,7 @@ export default function Header() {
       <div className="w-full mx-auto flex h-28 items-center justify-between px-4 md:px-6">
         <div className="flex items-center gap-10">
           <Link href="/" className="flex-shrink-0">
-              <div className="relative" style={{ height: '6.3rem', width: '13.2rem' }}>
+              <div className="relative" style={{ height: '6.3rem', width: '10rem' }}>
                 {logoUrl ? (
                     <Image
                         src={logoUrl}
